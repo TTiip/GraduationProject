790bcbd4f4d8579127d475bc03938302a1f530a6
TTiip <2711430287@qq.com>
2023-08-06 16:24:41 +0800

    feat: 商品分类列表页面

--- frontEnd/src/pages/goods3/index.tsx
+++ frontEnd/src/pages/goods3/index.tsx
@@ -1,4 +1,4 @@
-import { ElCard, ElIcon } from 'element-plus'
+import { ElButton, ElCard, ElIcon } from 'element-plus'
 import Table from '~/components/table'
 import { columnList, data } from '~/pages/goods3/data'
 
@@ -8,11 +8,16 @@ export default defineComponent({
     return () => (
       <div class="p-16px">
         <ElCard>
-          <div class="flex items-center">
-            <ElIcon style="font-size: 24px">
-              <i class="i-iconoir-task-list"></i>
-            </ElIcon>
-            <span class="ml-4px">数据列表</span>
+          <div class="flex items-center justify-between">
+            <div class="flex items-center">
+              <ElIcon style="font-size: 24px">
+                <i class="i-iconoir-task-list"></i>
+              </ElIcon>
+              <span class="ml-4px">数据列表</span>
+            </div>
+            <div>
+              <ElButton type="primary">添加</ElButton>
+            </div>
           </div>
         </ElCard>
         <Table
