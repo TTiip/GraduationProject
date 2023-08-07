266df43f252e6fed34e35d60e862103ca36ae019
TTiip <2711430287@qq.com>
2023-08-07 16:07:27 +0800

    feat: 用户列表

--- frontEnd/src/pages/user1/index.tsx
+++ frontEnd/src/pages/user1/index.tsx
@@ -1,15 +1,51 @@
+import { ElButton, ElCard, ElIcon } from 'element-plus'
+import Form from '~/components/from'
+import Table from '~/components/table'
+import { columnList, data } from '~/pages/user1/data'
+import { type typeFormItem } from '~/types/form'
+
 export default defineComponent({
-  name: 'index',
+  name: 'good1',
   setup () {
-    const num = ref<number>(0)
-    function addNum () {
-      num.value += 1
-    }
+    const model = reactive({
+      goodsName: ''
+    })
+
+    const rules = {}
+    const fromItemConfig: typeFormItem<keyof typeof model>[] = [
+      {
+        label: '账号/姓名',
+        key: 'goodsName',
+        type: 'Input',
+        attrs: {
+          placeholder: '请输入账号/姓名'
+        }
+      }
+    ]
     return () => (
-      <div>
-        <h1 onClick={ addNum }>
-          Index - { num.value }
-        </h1>
+      <div class="p-16px">
+        <Form
+          model={model}
+          rules={rules}
+          fromItemConfig={fromItemConfig}
+        />
+        <ElCard>
+          <div class="flex items-center justify-between">
+            <div class="flex items-center">
+              <ElIcon style="font-size: 24px">
+                <i class="i-iconoir-task-list"></i>
+              </ElIcon>
+              <span class="ml-4px">数据列表</span>
+            </div>
+            <div>
+              <ElButton type="primary">添加</ElButton>
+            </div>
+          </div>
+        </ElCard>
+        <Table
+          data={data}
+          columnList={columnList}
+        />
       </div>
     )
   }
